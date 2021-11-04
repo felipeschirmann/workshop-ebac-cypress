459f993ab2d83aca5505cbec6b30474b9c22a10d
Felipe Schirmann <felipeschirmann@gmail.com>
2021-11-04 00:17:51 -0300

    Automate the exclusion

--- cypress/integration/devfinance.spec.js
+++ cypress/integration/devfinance.spec.js
@@ -12,5 +12,21 @@ describe ('DevFinance', () => {
         cy.contains('button', 'Salvar').click()
 
         cy.get('table tbody tr').should('have.length', 1)
+
+        cy.get('a[onclick*=open]').click()
+        cy.get('#description').type('Chocolate')
+        cy.get('#amount').type('-3')
+        cy.get('#date').type('2021-11-03')
+
+        cy.contains('button', 'Salvar').click()
+
+        cy.get('table tbody tr').should('have.length', 2)
+
+        cy.get('#totalDisplay').contains('R$ 9,00')
+    }),
+
+    it('Remove transaction Chocolate', () => {
+        cy.get('table > tbody > tr > td > img').last().click()
+        cy.get('#totalDisplay').contains('R$ 12,00')
     })
-});
+})
